b2bd687963bfa2d8aa7bf0b524641fc44be907df
Иван Рогожин <IIROGOZHIN@GM040>
2022-11-20 17:48:08 +0300

    remove commits

--- seller-shop.Main/Program.cs
+++ seller-shop.Main/Program.cs
@@ -1,6 +1,30 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-WebApplication app = builder.Build();
+using seller_shop.Main.Extensions;
+using seller_shop.Main.ProjectCfg;
 
-app.MapGet("/", () => "Hello World!");
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+
+ProjectConfigurations projectConfigurations = new(builder.Configuration);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddClientPostgress(projectConfigurations);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapGet("/hi", () => "Hello!");
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 app.Run();

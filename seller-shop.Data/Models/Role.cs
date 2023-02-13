05b93852f81e98dc26400abd3ff737ed4d383eca
Иван Рогожин <IIROGOZHIN@GM040>
2023-02-13 03:10:13 +0300

    роли + его клейм

---
+++ seller-shop.Data/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace seller_shop.Data.Models;
+
+public class Role : IdentityRole<Guid>
+{
+    public new Guid Id { get; set; }
+    public Role? UserRole { get; set; }
+
+    public ICollection<RoleClaim> RoleClaims { get; set; } = new HashSet<RoleClaim>();
+    public ICollection<UserRole> UserRoles { get; set; } = new HashSet<UserRole>();
+
+}
---
+++ seller-shop.Data/Models/RoleClaim.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace seller_shop.Data.Models;
+
+public class RoleClaim : IdentityRoleClaim<Guid>
+{
+    public new Guid Id { get; set; } = Guid.NewGuid();
+    public Role? Role { get; set; }
+}

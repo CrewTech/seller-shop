55f5a9ef4a3abe8f03ba7caea64ef0779c61bcf3
Иван Рогожин <IIROGOZHIN@GM040>
2023-02-13 03:13:11 +0300

    add context

---
+++ seller-shop.Data/Context/SellerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using seller_shop.Data.Models;
+
+namespace seller_shop.Data.Context;
+
+public class SellerDbContext : IdentitySellerDbContext
+{
+    public DbSet<Category> Categories { get; set; } 
+    public DbSet<SubCategory> SubCategories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Discount> Discounties { get; set; }
+    public DbSet<Producer> Producers { get; set; }
+    public DbSet<ProducerDetail> ProducerDetails { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+
+    public SellerDbContext(DbContextOptions<DbContext> options) : base(options)
+    {
+
+    }
+}

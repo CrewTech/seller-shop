90df6db69b4292fa6568ad4cd7e4abd2b5a57edc
Иван Рогожин <IIROGOZHIN@GM040>
2023-02-13 03:07:09 +0300

    добавил сущности для подсчета стоимости заказа

---
+++ seller-shop.Data/Models/Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace seller_shop.Data.Models;
+
+public class Invoice
+{
+    public Guid Id { get; set; }
+    public Guid ApplicationUserId { get; set; }
+    public Guid OrderId { get; set; }
+
+    public decimal TotalValue { get; set; }
+
+    [Required]
+    public DateTime DateIssued { get; set; }
+
+    public User ApplicationUser { get; set; } = null!;
+    public Order Order { get; set; } = null!;
+
+    public ICollection<InvoiceDetail> Details { get; set; } = null!;
+}
---
+++ seller-shop.Data/Models/InvoiceDetail.cs
@@ -0,0 +1,15 @@
+﻿namespace seller_shop.Data.Models;
+
+public class InvoiceDetail
+{
+    public Guid Id { get; set; }
+    public Guid InvoiceId { get; set; }
+    public Guid ProductId { get; set; }
+
+    public int ItemQuantity { get; set; }
+
+    public decimal ItemPrice { get; set; }
+
+    public Invoice Invoice { get; set; } = null!;
+    public Product Product { get; set; } = null!;
+}

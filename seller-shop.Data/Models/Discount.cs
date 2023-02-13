01a1f8574df73f09daf7700ae057aa112f43bc10
Иван Рогожин <IIROGOZHIN@GM040>
2023-02-13 03:05:50 +0300

    добавил сущность для представления скидок

---
+++ seller-shop.Data/Models/Discount.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace seller_shop.Data.Models;
+
+/// <summary>
+/// Сущность скидки
+/// </summary>
+public class Discount
+{
+    public Guid Id { get; set; }
+
+    public Guid ProductID { get; set; }
+
+    [Required]
+    public decimal Percentage { get; set; }
+
+    public DateTime StartTime { get; set; }
+
+    [Required]
+    public DateTime EndTime { get; set; }
+
+    public Product Product { get; set; } = null!;
+}

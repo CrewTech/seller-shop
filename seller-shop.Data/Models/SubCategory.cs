509c78d89780d033328ed8779d210bcf45058fc8
Иван Рогожин <IIROGOZHIN@GM040>
2023-02-13 03:12:03 +0300

    подкатегория

---
+++ seller-shop.Data/Models/SubCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace seller_shop.Data.Models;
+
+/// <summary>
+/// подкатегории товара
+/// </summary>
+public class SubCategory
+{
+    public Guid Id { get; set; }
+    public Guid CategoryID { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string Name { get; set; } = null!;
+
+    public Category Category { get; set; } = null!;
+}

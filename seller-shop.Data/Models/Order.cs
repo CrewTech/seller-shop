d8a7bc992e2f621d749b6bb4731e97fcaec8e75f
Иван Рогожин <IIROGOZHIN@GM040>
2023-02-13 03:07:23 +0300

    ордера

---
+++ seller-shop.Data/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace seller_shop.Data.Models;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public Guid ApplicationUserId { get; set; }
+    public Guid ShippingMethodId { get; set; }
+
+    [Required]
+    public DateTime DateAndTime { get; set; }
+
+    [StringLength(400)]
+    public string Note { get; set; } = null!;
+
+    public decimal ShippingMethodPrice { get; set; }
+
+    public User ApplicationUser { get; set; } = null!;
+    public ShippingMethod ShippingMethod { get; set; } = null!;
+    public ICollection<OrderDetail> Details { get; set; } = null!;
+    public ICollection<Invoice> Invoices { get; set; } = null!;
+
+}

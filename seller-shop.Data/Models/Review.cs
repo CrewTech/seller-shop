60733ad056e73e4be38a60803dd5b924651bcb61
Иван Рогожин <IIROGOZHIN@GM040>
2023-02-13 03:10:00 +0300

    отзыв

---
+++ seller-shop.Data/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace seller_shop.Data.Models;
+
+public class Review
+{
+    public Guid Id { get; set; }
+    public Guid ApplicationUserId { get; set; }
+    public Guid ProductId { get; set; }
+
+    [Required]
+    [Range(1, 6)]
+    public int Rating { get; set; }
+
+    [Required]
+    [StringLength(1024)]
+    public string Content { get; set; } = string.Empty;
+
+    public User ApplicationUser { get; set; } = null!;
+    public Product Product { get; set; } = null!;
+}

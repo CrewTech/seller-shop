82a3f61b62fb59bedca78279e95db0b4bb2e53fd
Иван Рогожин <IIROGOZHIN@GM040>
2023-02-13 03:14:28 +0300

    add DbContext to DI

--- seller-shop.Main/Extensions/ClientPostgres.cs
+++ seller-shop.Main/Extensions/ClientPostgres.cs
@@ -1,4 +1,6 @@
-﻿using seller_shop.Main.ProjectCfg;
+﻿using Microsoft.EntityFrameworkCore;
+using seller_shop.Data.Context;
+using seller_shop.Main.ProjectCfg;
 
 namespace seller_shop.Main.Extensions;
 
@@ -7,6 +9,7 @@ public static class ClientPostgres
     public static void AddClientPostgreSql(this IServiceCollection services, ProjectConfigurations configurations)
     {
         string connectionString = configurations.ConnectionStringPsql;
-        ;
+
+        services.AddDbContext<SellerDbContext>(options => options.UseNpgsql(connectionString));
     }
 }

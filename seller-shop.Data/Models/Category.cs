b2dc237c020caba2ba4c32903fffc171138dd8d7
Иван Рогожин <IIROGOZHIN@GM040>
2023-02-13 03:05:27 +0300

    добавил сущность категории

---
+++ seller-shop.Data/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace seller_shop.Data.Models;
+
+/// <summary>
+/// Сущность представляет категории товара
+/// </summary>
+public class Category
+{
+    public Guid Id { get; set; }
+    public ICollection<SubCategory>? SubCategories { get; set; }
+}

de5e9e012d147ea38f046be6d7982081df9f5bd2
Иван Рогожин <IIROGOZHIN@GM040>
2023-02-13 03:09:30 +0300

    производитель

---
+++ seller-shop.Data/Models/Producer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace seller_shop.Data.Models;
+
+public class Producer
+{
+    public Guid Id { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string Name { get; set; } = null!;
+
+    public ICollection<ProducerDetail> Details = null!;
+}
---
+++ seller-shop.Data/Models/ProducerDetail.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace seller_shop.Data.Models;
+
+public class ProducerDetail
+{
+    public Guid Id { get; set; }
+
+    public Guid ProducerId { get; set; }
+
+    [StringLength(30)]
+    public string PhoneNumber { get; set; } = null!;
+
+    [StringLength(200)]
+    public string Email { get; set; } = null!;
+
+    [StringLength(200)]
+    public string Address1 { get; set; } = null!;
+
+    [StringLength(200)]
+    public string Address2 { get; set; } = null!;
+
+    [StringLength(200)]
+    public string City { get; set; } = null!;
+
+    [StringLength(200)] 
+    public string Country { get; set; } = null!;
+
+    public Producer Producer { get; set; } = null!;
+}
